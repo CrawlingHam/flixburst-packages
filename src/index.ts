045f39e427429489a4df547ccf9bcdb36e2e5e6b
CrawlingHam <crawlingham@gmail.com>
2025-04-23 19:23:40 +0200

    fixed bugs

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,23 @@
-export {
-    envCache,
-    envSources,
-    clearCache,
-    constructUrl,
-    getVariableSource,
-    getEnvironmentVariable,
+import {
     validateEnvironmentVariable,
+    getEnvironmentVariable,
     ensureRequiredEnvironmentVariables,
+    clearCache,
+    getVariableSource,
 } from './utils';
+
+export {
+    validateEnvironmentVariable,
+    getEnvironmentVariable,
+    ensureRequiredEnvironmentVariables,
+    clearCache,
+    getVariableSource,
+};
+
+export { envCache, envSources, constructUrl } from './utils';
+
 export { BaseEnvironment, Environment } from './classes';
+
 export type {
     IStandardEnvironmentValidator,
     IEnvironmentProcessor,
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,7 +1,2 @@
-export {
-    constructUrl,
-    getEnvironmentVariable,
-    validateEnvironmentVariable,
-    ensureRequiredEnvironmentVariables,
-} from './class.utils';
-export { clearCache, envCache, envSources, getVariableSource } from './cache.utils';
+export * from './cache.utils';
+export * from './class.utils';
